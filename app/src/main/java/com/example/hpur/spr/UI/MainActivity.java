fd256ddf2fe9f89699b8e80a3b8a6d8ffc902591
Hadar Pur <hpur@liveperson.com>
2018-12-27 18:25:13 +0200

    manifest fixed

--- app/src/main/java/com/example/hpur/spr/UI/MainActivity.java
+++ app/src/main/java/com/example/hpur/spr/UI/MainActivity.java
@@ -7,14 +7,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
-
 import com.example.hpur.spr.Logic.GPSTracker;
 import com.example.hpur.spr.Logic.Shelter;
 import com.example.hpur.spr.Logic.ShelterInstance;
