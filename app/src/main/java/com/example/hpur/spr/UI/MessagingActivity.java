1d1aaf5eb23f4c675c0adf4c84f88e9b7be35eca
Hadar Pur <hpur@liveperson.com>
2018-12-28 11:50:49 +0200

    hadar's little changes

--- app/src/main/java/com/example/hpur/spr/UI/MessagingActivity.java
+++ app/src/main/java/com/example/hpur/spr/UI/MessagingActivity.java
@@ -24,14 +24,15 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.ImageButton;
 
 
 public class MessagingActivity extends AppCompatActivity {
 
     private final String TAG = "MessagingActivity:";
-    private ListView listView;
-    private View btnSend;
-    private EditText editText;
+    private ListView mListView;
+    private View mSendBtn;
+    private EditText mEditText;
     boolean mMyMessage = true;
     private String mUserName = "Anonymouse";
     private List<ChatBubble> mChatBubbles;
@@ -41,64 +42,97 @@ public class MessagingActivity extends AppCompatActivity {
     private DatabaseReference mMessagesDatabaseReference;
     private ChildEventListener mChildEventListener;
 
+    private ImageButton mPhone;
+    private ImageButton mVideo;
+    private ImageButton mBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        this.mFirebaseDatabase = FirebaseDatabase.getInstance();
+        this.mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
 
-        mChatBubbles = new ArrayList<>();
+        this.mChatBubbles = new ArrayList<>();
+
         findViews();
+        setupOnClick();
+        setListener();
 
         //set ListView adapter first
-        mChatAdapter = new MessageAdapter(MessagingActivity.this, R.layout.left_chat_bubble, mChatBubbles);
-        listView.setAdapter(mChatAdapter);
+        this.mChatAdapter = new MessageAdapter(MessagingActivity.this, R.layout.left_chat_bubble, mChatBubbles);
+        this.mListView.setAdapter(mChatAdapter);
 
-        initListeners();
-        mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        this.mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
     }
 
     public void onBackPressed(){
-        new AlertDialog.Builder(this)
-                .setTitle("Really Exit?")
-                .setMessage("Are you sure you want to leave the chat?")
-                .setNegativeButton(android.R.string.no, null)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface arg0, int arg1) {
-                       MessagingActivity.super.onBackPressed();
-                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                    }
-                }).create().show();
+        alertGotOut();
     }
 
     private void findViews(){
-        listView = (ListView) findViewById(R.id.list_msg);
-        btnSend = findViewById(R.id.btn_chat_send);
-        editText = (EditText) findViewById(R.id.msg_type);
+        this.mListView = findViewById(R.id.list_msg);
+        this. mSendBtn = findViewById(R.id.btn_chat_send);
+        this.mEditText = findViewById(R.id.msg_type);
+
+        this.mPhone = findViewById(R.id.phone);
+        this.mVideo = findViewById(R.id.video);
+        this.mBack = findViewById(R.id.backbtn);
+
+        this.mPhone.setVisibility(View.VISIBLE);
+        this.mVideo.setVisibility(View.VISIBLE);
+
+        this.mPhone.setClickable(true);
+        this.mVideo.setClickable(true);
     }
 
-    private void initListeners(){
-
-        //event for button SEND
-        btnSend.setOnClickListener(new View.OnClickListener() {
+    // setup all button events when they clicked
+    private void setupOnClick() {
+        this.mPhone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText.getText().toString().trim().equals("")) {
-                    Toast.makeText(MessagingActivity.this, "Please input some text...", Toast.LENGTH_SHORT).show();
-                } else {
-                    //add message to list
-                    ChatBubble chatBubble = new ChatBubble(editText.getText().toString(), mUserName, mMyMessage);
-                    mMessagesDatabaseReference.push().setValue(chatBubble);
-                    editText.setText("");
-                }
+                Toast.makeText(MessagingActivity.this, "mPhone clicked", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+        this.mVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MessagingActivity.this, "mVideo clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        this.mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
+        //event for button SEND
+        this.mSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditText.getText().toString().trim().equals("")) {
+                    Toast.makeText(MessagingActivity.this, "Please input some text...", Toast.LENGTH_SHORT).show();
+                } else {
+                    //add message to list
+                    ChatBubble chatBubble = new ChatBubble(mEditText.getText().toString(), mUserName, mMyMessage);
+                    mMessagesDatabaseReference.push().setValue(chatBubble);
+                    mEditText.setText("");
+                }
+            }
+        });
+    }
+
+
+    private void setListener() {
         //Notify each time there is a change in messaging node in the DB
-        mChildEventListener = new ChildEventListener() {
+        this.mChildEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 //Called in the start for each child in the node + every time a child inserted to the DB
@@ -139,76 +173,20 @@ public class MessagingActivity extends AppCompatActivity {
 
             }
         };
+    }
+    private void alertGotOut() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to leave the chat?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MessagingActivity.super.onBackPressed();
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                    }
+                }).create().show();
     }
 }
 
-    /*
 
-    import android.os.Bundle;
-    import android.view.View;
-    import android.widget.ImageButton;
-    import android.widget.Toast;
-    import com.example.hpur.spr.R;
-
-
-    private ImageButton mPhone;
-    private ImageButton mVideo;
-    private ImageButton mBack;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messaging);
-
-        findViews();
-        setupOnClick();
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-    }
-
-    //find views from xml with listeners
-    private void findViews() {
-        this.mPhone = findViewById(R.id.phone);
-        this.mVideo = findViewById(R.id.video);
-        this.mBack = findViewById(R.id.backbtn);
-
-        this.mPhone.setVisibility(View.VISIBLE);
-        this.mVideo.setVisibility(View.VISIBLE);
-
-        this.mPhone.setClickable(true);
-        this.mVideo.setClickable(true);
-    }
-
-    // setup all button events when they clicked
-    private void setupOnClick() {
-        this.mPhone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MessagingActivity.this, "mPhone clicked", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        this.mVideo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MessagingActivity.this, "mVideo clicked", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        this.mBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-            }
-        });
-    }
-
-*/
