6c2bc67c2e477a9d34e47a9b65efcc95fc60567b
hpur <hpur@liveperson.com>
2019-05-14 12:04:04 +0300

    Add progress bar till the map load

--- app/src/main/java/com/example/hpur/spr/Logic/ChatBubbleHolder.java
+++ app/src/main/java/com/example/hpur/spr/Logic/ChatBubbleHolder.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.hpur.spr.Logic.Queries.OnMapClickedCallback;
 import com.example.hpur.spr.R;
+import com.github.ybq.android.spinkit.SpinKitView;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -25,6 +26,8 @@ public class ChatBubbleHolder extends RecyclerView.ViewHolder {
     private Context mContext;
     private ChatBubble mChatBubble;
     private FragmentManager mFragment;
+    private SpinKitView mProgressBar = null;
+
 
     public ChatBubbleHolder(FragmentManager fragment, Context context, @NonNull View itemView, int type) {
         super(itemView);
@@ -37,6 +40,8 @@ public class ChatBubbleHolder extends RecyclerView.ViewHolder {
                 break;
             case USER_MAP: case OTHER_MAP:
                 this.mMapMessage = itemView.findViewById(R.id.map_bubble);
+                this.mProgressBar = itemView.findViewById(R.id.spin_kit);
+
                 break;
         }
     }
@@ -44,18 +49,25 @@ public class ChatBubbleHolder extends RecyclerView.ViewHolder {
     public void bindChatBubble(ChatBubble chatBubble, final OnMapClickedCallback callback){
         this.mChatBubble = chatBubble;
         if (mChatBubble.getmMapModel() != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
             final String latLong = mChatBubble.getmMapModel().getLongitude() +","+mChatBubble.getmMapModel().getLatitude();
             final String url = "https://static-maps.yandex.ru/1.x/?lang=en_US&ll="+latLong+"&size=400,200&z=7&l=map&pt="+latLong+",pm2rdl";
             Picasso.get().load(url).fit().into(mMapMessage, new Callback() {
                 @Override
                 public void onSuccess() {
                     Log.d("bindChatBubble", "onSuccess");
+                    if (mProgressBar != null) {
+                        mProgressBar.setVisibility(View.GONE);
+                    }
                 }
 
                 @Override
                 public void onError(Exception e) {
                     Log.d("bindChatBubble", "onError: "+ e);
                     e.printStackTrace();
+                    if (mProgressBar != null) {
+                        mProgressBar.setVisibility(View.GONE);
+                    }
                 }
             });
             this.mMapMessage.setOnClickListener(new View.OnClickListener() {
