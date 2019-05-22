880adbc07dcbb11399498cd2c284c18428e1d7d0
hpur <hpur@liveperson.com>
2019-05-22 22:40:16 +0300

    Change the local url to the external one

--- app/src/main/java/com/example/hpur/spr/UI/Utils/OpenTokConfig.java
+++ app/src/main/java/com/example/hpur/spr/UI/Utils/OpenTokConfig.java
@@ -1,37 +1,17 @@
 package com.example.hpur.spr.UI.Utils;
 
-
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.text.format.Formatter;
 import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.hpur.spr.Logic.Queries.TokBoxServerSDKCallback;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Enumeration;
-
-import static android.content.Context.WIFI_SERVICE;
-import static org.webrtc.ContextUtils.getApplicationContext;
 
 public class OpenTokConfig {
 
@@ -48,10 +28,9 @@ public class OpenTokConfig {
     }
 
     public void tokboxHttpJsonRequest(final TokBoxServerSDKCallback callback) {
-        String ip = "172.40.1.47";
-        String JsonDevice = "http://"+ip+":5000";
+        String httpUrl = "https://us-central1-sprfinalproject.cloudfunctions.net/opentokSessionId";
 
-        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, JsonDevice,
+        JsonObjectRequest obreq = new JsonObjectRequest(Request.Method.GET, httpUrl,
                 new Response.Listener<JSONObject>() {
 
                     @Override
